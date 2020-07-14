543fae1765604f9cd3c2d91c0fc9805106d30919
sharsha2 <36820319+sharsha2@users.noreply.github.com>
2020-07-14 06:43:02 +0530

    Add files via upload
    updated files

--- go-go-grocery-application-start/xhrapi.js
+++ go-go-grocery-application-start/xhrapi.js
@@ -1,35 +1,50 @@
- let dealsSection = document.getElementById("deals");
+/* STEP 3: Store the URL of a JSON file in a variable */
+let dealsSection = document.getElementById('deals');
+let requestURL = 'https://sharsha2.github.io/API-Json/go-go-grocery-application-start/dailydeals.json';
+/* STEP 4: Create a new XHR object */
+let request = new XMLHttpRequest();
+/* STEP 5: Open a new request using the request() method */
+request.open('GET', requestURL);
+/* STEP 6: Set JavaScript to accept JSON from the server */
+request.responseType = 'json';
+/* STEP 7: Send the request with the send() method */
+request.send();
+/* STEP 8: Add an event handler that listens for the onload event of the JSON file/object */
+request.onload = function() {
+  let dailyDeals = request.response;
+  console.log(dailyDeals);
+  dealInfo(dailyDeals);
+};
 
- //store the request URL.
- let requestURL = "https://sharsha2.github.io/API-Json/dailydeals.json";
+/* STEP 10a: Assemble the showTopFlavors() function */
+function dealInfo(jsonObj){ 
+  // STEP 10b: Bind the JSON topFlavors object to a var
+  let dealInfo = jsonObj['dailyDeals'];
+  // STEP 10c: Loop through the topFlavors object
+  for (let i = 0; i < dealInfo.length; i++) {
+    // STEP 10d: build HTML elements for the content
+    let div = document.createElement('div'); 
+    div.setAttribute('class', 'col-md-3'); 
+    let description  = document.createElement('h3'); 
+    let price  = document.createElement('p');
+    let image = document.createElement('img');  
 
- //create a new XHR object
- let request = new XMLHttpRequest();
-
- //open the request
- request.open('GET', requestURL);
-
- //setup responce type
- request.responseType = 'json';
-
- //send the request using send();
- request.send();
-
- request.onload = function () {
-     let dailyDeals = request.response;
-     console.log(dailyDeals);
-     dealInfo(dailyDeals);
- };
-
- dealInfo(jsonObj) {
-     let dalInfo = jsonObj['dailyDeals'];
-     for(let i=0;i< dealInfo.length;i++) {
-         let dev = document.createElement('div');
-         Div.setAttribute('class', 'col-md-3');
-        let description = document.createElement('h3');
-        let price = document.createElement('p');
-        let image = document.createElement('img');
-
-        image.setAttribute('src', '')
+    // STEP 10e: Set the textContent property for each of the above elements (except the UL), based on the JSON content
+    image.setAttribute('src', 'https://sharsha2.github.io/API-Json/go-go-grocery-application-start/images/' + dealInfo[i].image);
+    image.setAttribute('alt', dealInfo[i].item);
+    description.textContent = dealInfo[i].item;
+    price.textContent = dealInfo[i].price;
+    
+    div.appendChild(description);
+    div.appendChild(price);
+    div.appendChild(image);
+    dealsSection.appendChild(div); 
     }
- }
+  }
+// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+/* 6. 
+Modern browsers will not run XHR requests if you just run the example from a local file. This is because of security restrictions (for more on web security, read Website security).*/
+
+/* This code inspired by - 
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+*/
